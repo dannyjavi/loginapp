24345f13bf90a1087ce92d9cd29a2b10c3965fa6
maria <dannyja8@hotmail.com>
2018-03-12 09:23:58 +0100

    Creacion de componentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './componentes/homepage/homepage.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { RegisterPageComponent } from './componentes/register-page/register-page.component';
+import { PrivadoPageComponent } from './componentes/privado-page/privado-page.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    RegisterPageComponent,
+    PrivadoPageComponent,
+    NotFoundPageComponent,
+    LoginPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
